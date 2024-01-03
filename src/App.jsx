886f064a57a2f1e92083a1711011709d15575922
Anna N Helms <unicornbuddha@gmail.com>
2024-01-02 16:17:00 -0800

    add logout link

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from "./Header";
 import { Content } from "./Content";
 import MyMap from "./MyMap";
 import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 import { Signup } from "./Signup";
 import { Footer } from "./Footer";
 
@@ -12,6 +13,7 @@ function App() {
       <Content />
       <MyMap />
       <Login />
+      <LogoutLink />
       <Signup />
       <Footer />
     </div>
