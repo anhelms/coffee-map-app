02afc00d7c6af918d688cb8649102650515956f9
Anna N Helms <unicornbuddha@gmail.com>
2024-01-10 17:26:46 -0800

    fixes map onlick

--- src/MyMap.jsx
+++ src/MyMap.jsx
@@ -1,39 +1,51 @@
 import React, { useState, useEffect } from 'react'; //eslint-disable-line
 import { Map, Marker } from 'pigeon-maps';
 import { Modal } from "./Modal";
+import { CoffeeShopsShow } from './CoffeeShopShow';
 
 const MyMap = ( { coffeeShops }) => {
   console.log('Coffee Shops:', coffeeShops);
+  const [coffeeShopToShow, setCoffeeShopToShow] = useState(null);
+
+  const handleMarkerClick = (coffeeShop) => {
+     console.log('Clicked coffeeShop:', coffeeShop);
+    setCoffeeShopToShow(coffeeShop);
+  };
+
+  const handleCloseModal = () => {
+    setCoffeeShopToShow(null);
+  };
+
   if (!coffeeShops) {
     return null;
   }
-
-  const [popoverVisible, setPopoverVisible] = useState(false);
-  const [popoverContent, setPopoverContent] = useState(undefined);
-
-  useEffect(() => {
-    console.log('Inside useEffect - Popover Visible:', popoverVisible);
-  }, [popoverVisible]);
-
-  const handleMarkerClick = (coffeeShop) => {
-    console.log('Before state update - Popover Visible:', popoverVisible);
-    setPopoverVisible(true);
-    setPopoverContent(coffeeShop.name);
-    console.log('After state update - Popover Visible:', popoverVisible);
-  };
 
   return (
     <div className="map-container">
       <Map center={[34.0549, -118.2426]} zoom={12} width={1000} height={400}>
         {coffeeShops.map(coffeeShop => (
-          <Marker key={coffeeShop.id} anchor={[parseFloat(coffeeShop.latitude), parseFloat(coffeeShop.longitude)]}  color='red' onClick={() => handleMarkerClick(coffeeShop)} width={75}>
-            <img src="src/images/coffee-ping.png" alt="coffee marker"/>
+          <Marker
+            key={coffeeShop.id} 
+            anchor={[parseFloat(coffeeShop.latitude), parseFloat(coffeeShop.longitude)]} 
+            color='red' 
+            width={75}
+            onClick={() => {
+              console.log('Marker clicked:', coffeeShop);
+              handleMarkerClick(coffeeShop);
+            }}
+            >
+            {/* <img src="src/images/coffee-ping.png" alt="coffee marker"/> */}
           </Marker>
         ))}
-        <Modal show={popoverVisible} onClose={() => setPopoverVisible(false)}>
-          <div>This is a test - I clicked on {popoverContent}</div>
-        </Modal>
       </Map>
+      {coffeeShopToShow && (
+        <Modal show={true} onClose={handleCloseModal}>
+            {/* <p>Name: {coffeeShopToShow.name}</p>
+            <p>Latitude: {coffeeShopToShow.latitude}</p>
+            <p>Longitude: {coffeeShopToShow.longitude}</p> */}
+            <CoffeeShopsShow coffeeShop={coffeeShopToShow} />
+        </Modal>
+      )}
     </div>
   );
 };
