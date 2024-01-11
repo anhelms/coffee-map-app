60dc6c827e6d5d26c7d7744b3b08148b8f2a12fc
Anna N Helms <unicornbuddha@gmail.com>
2024-01-10 20:43:23 -0800

    signup edits

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -30,10 +30,16 @@ export function Signup() {
                     Name: <input name="name" type="text" />
                 </div>
                 <div className="form-group">
+                    Email: <input name="email" type="text" />
+                </div>
+                <div className="form-group">
                     Password: <input name="password" type="password" />
                 </div>
                 <div className="form-group">
                     Password confirmation: <input name="password_confirmation" type="password" />
+                </div>
+                <div className="form-group">
+                    Profile Picture: <input name="image_url" type="file" onChange={handleSetFile} />
                 </div>
                 <button type="submit">Signup</button>
             </form>
