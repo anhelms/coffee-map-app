3be95fb1eef7cb51961e74573e35ecd89b7d1bf4
Anna N Helms <unicornbuddha@gmail.com>
2024-01-23 15:46:36 -0800

    add demo account

--- src/DemoAccount.jsx
+++ src/DemoAccount.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import axios from 'axios';
 
 const DemoAccount = () => {
-  const handleCreateDemoAccount = async () => {
-    try {
-      const response = await axios.post('/create_demo_account');
-      // Handle success, store token, etc.
-    } catch (error) {
-      // Handle error
-    }
+  const handleCreateDemoAccount = () => {
+    let params = {email: "tiger@test.com", password: "password"}
+    axios
+      .post("/sessions.json", params)
+      .then((response) => {
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        window.location.reload();
+      })
   };
 
   return (
